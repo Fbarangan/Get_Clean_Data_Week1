e793f2c3ca9a223ff01c7c6edd6e829510538c1f
Fbarangan <franb86.2@gmail.com>
2016-02-06 16:35:16 -0500

    3rd commit

--- downloadinf_csv_url.R
+++ downloadinf_csv_url.R
@@ -41,4 +41,24 @@
         fileUrl <- "http://www.w3schools.com/xml/simple.xml"
         doc <- xmlTreeParse(fileUrl, useInternal = TRUE )
         rootNode <- xmlRoot(doc)
-       names(rootNode)
+# will extract the first items(wrap)
+        names(rootNode)
+        rootNode[[1]]
+        rootNode[[1]][[1]]
+
+#Programmatically extract files
+        xmlSApply(rootNode, xmlValue)
+# Get items on the menu prices
+        xpathSApply(rootNode, "//name", xmlValue)
+        xpathSApply(rootNode, "//price", xmlValue)
+
+# NFL example
+#  sample - http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens
+#  Go Bengals !!!
+# ue html instead of XML parse
+        fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+        doc <- htmlTreeParse(fileUrl, useInternal = TRUE)
+# The example on the lecture use "li" but this may change depending on the updated
+# website. currently the scores can be found using "div"
+        scores <- xpathSApply(doc, "//div[@class='score']", xmlValue)
+        teams <- xpathSApply(doc, "//div[@class = 'game-info']", xmlValue)
